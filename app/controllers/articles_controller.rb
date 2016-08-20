433e2e4c5be160dd766877b0becb754a002a95e3
aaronleechan <aaron171091@gmail.com>
2016-08-19 18:29:25 -0700

    create new tempelate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,20 @@ class ArticlesController < ApplicationController
 	def new
 		@article = Article.new
 	end
+
+	def create
+		#render plain: params[:article].inspect
+		@article = Article.new(article_params)
+		@article.save
+		redirect_to articles_show(article)
+	end
+
+
+# that is save
+	private
+	def article_params
+		params.require(:article).permit(:title, :description)
+	end
+
 	
 end
